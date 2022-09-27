6f0fde90890308b0316b31cd45e5d4965e9b7890
edgar <edgar@DESKTOP-69TJ7NA>
2022-09-27 18:52:58 +0200

    video textures tweaks

--- src/Components/Room/index.jsx
+++ src/Components/Room/index.jsx
@@ -1,10 +1,14 @@
 import React , { useEffect } from 'react'
 import ScreenTexture from "../../assets/textures/screen_video.mp4"
+import ArcadeScreenTexture from "../../assets/textures/arcade_video.mp4"
 import * as THREE from 'three'
 import RoomModel from '../../assets/models/portfolio-room-arcade.glb'
 import { useAnimations, useGLTF, useVideoTexture} from '@react-three/drei'
 import { useRef } from 'react'
 
+const findMesh = (scene, name) => {
+  return scene.children[scene.children.findIndex((value) => value.name === name)]
+}
 
 
 function Room() {
@@ -12,20 +16,22 @@ function Room() {
   const room = useGLTF(RoomModel)
   
   const PCvideo = useVideoTexture(ScreenTexture)
-  const PCScreen = useRef(room.scene.children[room.scene.children.findIndex((value) => value.name === 'screen001')])
-  const Tesseract = useRef(room.scene.children[room.scene.children.findIndex((value) => value.name === 'Cube099')])
+  const Arcadevideo = useVideoTexture(ArcadeScreenTexture)
+  const PCScreen = useRef(findMesh(room.scene, "screen001" ))
+  const ArcadeScreen = useRef(findMesh(room.scene, "arcadeScreen001"))
+  const Tesseract = useRef(findMesh(room.scene, 'Cube099'))
   const animation = useAnimations(room.animations, Tesseract)
     console.log(room);
     useEffect(() => {
 
       room.scene.scale.set(0.11, 0.11, 0.11)
+     
       room.scene.children.forEach( child => {
-      
 
         if(child instanceof THREE.Group){
           child.children.forEach((groupchild) => {
             groupchild.castShadow = true
-            groupchild.receiveShadow = true
+            groupchild.receiveShadow = true 
         })
         }
         else
@@ -40,8 +46,9 @@ function Room() {
     
     useEffect(() => {
       PCScreen.current.material = new THREE.MeshBasicMaterial({map: PCvideo})
+      ArcadeScreen.current.material = new THREE.MeshBasicMaterial({map: Arcadevideo})
       animation.actions['Cube.099Action.001']?.play()
-    }, [PCvideo])
+    }, [PCvideo, Arcadevideo])
     
   return (
         <>  
